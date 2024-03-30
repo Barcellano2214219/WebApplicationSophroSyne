57e659bc2fab66f05a60479245964870e0512c80
Dave14 <Dave14@gmail.com>
2024-03-31 01:30:08 +0900

    Updates for the About Us HTML

--- script.js
+++ script.js
@@ -10,30 +10,30 @@ function clearSearch() {
 
 // Upload Button Clicked
 function openFileDialog() {
-    document.getElementById('uploadFileInput').click();
-  }
-  
-  document.getElementById('uploadFileInput').addEventListener('change', function(event) {
-    var file = event.target.files[0];
-    if (file) {
-      var fileName = file.name;
-      if (file.type === 'text/plain') {
-        readAsText(file);
-      } else {
-        alert('Only text files are supported.');
-      }
+  document.getElementById('uploadFileInput').click();
+}
+
+document.getElementById('uploadFileInput').addEventListener('change', function(event) {
+  var file = event.target.files[0];
+  if (file) {
+    var fileName = file.name;
+    if (file.type === 'text/plain') {
+      readAsText(file);
+    } else {
+      alert('Only text files are supported.');
     }
-  });
-  
-  function readAsText(file) {
-    var reader = new FileReader();
-    reader.onload = function(event) {
-      openInNewWindow(event.target.result);
-    };
-    reader.readAsText(file);
   }
-  
-  function openInNewWindow(text) {
-    var newWindow = window.open("about:blank", "_blank");
-    newWindow.document.write(text);
-  }
+});
+
+function readAsText(file) {
+  var reader = new FileReader();
+  reader.onload = function(event) {
+    openInNewWindow(event.target.result);
+  };
+  reader.readAsText(file);
+}
+
+function openInNewWindow(text) {
+  var newWindow = window.open("about:blank", "_blank");
+  newWindow.document.write(text);
+}
